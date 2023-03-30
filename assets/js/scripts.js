80d4f37ddc42ddb778c1e4e246dc21bd2c51d4ea
enespusa <pusaenes@gmail.com>
2023-03-30 11:25:01 +0300

    added JavaScript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,23 @@
-/*!
-* Start Bootstrap - Resume v7.0.3 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+
+
+function mouseOver( x) {
+    var element = document.getElementById("name");
+
+    element.style.color = "green";
+    element.innerText = x;
+
+}                       
+
+function mouseOut() {
+    var element = document.getElementById("name");
+
+    element.style.color = "red";
+    element.innerText = "ENES PUSA";
+
+}
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
@@ -18,7 +26,6 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     };
 
-    // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
